74beb1b7839cdcd586538454788fd738c479d44c
eskimo1228 <eskimo1228@outlook.com>
2020-04-20 19:16:48 +0300

    update all

---
+++ meta.js
@@ -0,0 +1,30 @@
+module.exports = {
+  helpers: {
+    raw: function(options) {
+      return options.fn(this)
+    }
+  },
+  prompts: {
+    name: {
+      'type': 'string',
+      'required': true,
+      'message': 'Project name'
+    },
+    description: {
+      'type': 'string',
+      'required': false,
+      'message': 'Project description',
+      'default': 'Nuxt.js + Vuetify.js project'
+    },
+    author: {
+      'type': 'string',
+      'message': 'Author'
+    },
+    alacarte: {
+      'type': 'confirm',
+      'message': 'Use a-la-carte components?',
+      'default': false
+    }
+  },
+  completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
+};
